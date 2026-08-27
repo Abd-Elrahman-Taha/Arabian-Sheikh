f61898ee2b9ad06ccc99a87a764fd88d3fe465ff
Abdelrahman Taha <b0ody66taha@gmail.com>
2026-08-27 16:58:05 +0300

    Api's

--- src/services/productService.js
+++ src/services/productService.js
@@ -2,7 +2,7 @@ import { INITIAL_PRODUCTS, PERFUME_TIERS, CATEGORIES } from './mockData';
 import { productApi } from '../api/product.api';
 import { apiClient } from '../api/client';
 
-const PRODUCTS_STORAGE_KEY = 'arabian_sheikh_api_products_v2';
+const PRODUCTS_STORAGE_KEY = 'arabian_sheikh_api_products_v4';
 let inMemoryProducts = [];
 
 function preloadProductAssets(products) {
